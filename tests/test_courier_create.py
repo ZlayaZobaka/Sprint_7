418e7bb7b950188cbc378fb9c7417aac34d3de64
Dmitry Sytnik <zlaya.zobaka@yandex.ru>
2024-05-03 18:25:26 +0300

    тесты создания курьера

--- tests/test_courier_create.py
+++ tests/test_courier_create.py
@@ -1,12 +1,41 @@
+import pytest
+from wrapper.courier import Courier
+
+
 class TestCourierCreate:
     # успешный запрос возвращает {"ok":true}
     def test_create_courier_correct_data_return_ok(self):
-        pass
+        courier = Courier()
+        payload = courier.create_new_courier_payload()
+
+        response = courier.create_new_courier(payload)
+
+        assert response.status_code == 201 and response.text == '{"ok":true}'
 
     # если создать пользователя с логином, который уже есть, возвращается ошибка Сonflict
     def test_create_courier_same_login_return_conflict_error(self):
-        pass
+        courier = Courier()
+        payload = courier.create_new_courier_payload()
+
+        courier.create_new_courier(payload)
+        new_response = courier.create_new_courier(payload)
+
+        assert (new_response.status_code == 409 and
+                new_response.json()['message'] == 'Этот логин уже используется. Попробуйте другой.')
 
     # если одного из полей нет, запрос возвращает ошибку Bad Request
-    def test_create_courier_lack_data_return_bad_request_error(self):
-        pass
+    @pytest.mark.parametrize(
+        "login, password",
+        [
+            [False, True],
+            [True, False]
+        ]
+    )
+    def test_create_courier_lack_data_return_bad_request_error(self, login, password):
+        courier = Courier()
+        payload = courier.create_new_courier_payload(login=login, password=password)
+        response = courier.create_new_courier(payload)
+
+        assert (response.status_code == 400 and
+                response.json()['message'] == 'Недостаточно данных для создания учетной записи')
+        
---
+++ wrapper/courier.py
@@ -0,0 +1,26 @@
+from wrapper.base import Base
+import requests
+import data
+class Courier(Base):
+    def __init__(self):
+        self.URL = f'{data.BASE_URL}/courier'
+
+    def create_new_courier_payload(self, login=True, password=True):
+        payload = {
+            "firstName": self.generate_random_string(10)
+        }
+        if login:
+            payload['login'] = self.generate_random_string(10)
+
+        if password:
+            payload['password'] = self.generate_random_string(10)
+
+        return payload
+
+    # отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response
+    def create_new_courier(self, payload):
+        response = requests.post(
+            url=self.URL,
+            data=payload)
+
+        return response
